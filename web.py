dd1e2b48a7b5a4ded0b8387ae1900c76ea3179f3
Md Alam <alamwebtech@gmail.com>
2019-02-11 19:52:13 -0500

    triyng to extract data

---
+++ web.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+response = requests.get("https://www.rithmschool.com/blog")
+soup = BeautifulSoup(response.text, "html.parser")
+articles = soup.find_all("artcle")
+for article in articles:
+	print(article.find("a").get_text())
